52f8e2c915c379c62298bebeb1cea9d112d9824c
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-03 12:13:33 +0530

    week 1 added

---
+++ Week 1/MarkAverage.java
@@ -0,0 +1,9 @@
+public class MarkAverage {
+	public static void main(String[] args) {
+		int mark1 = 46;
+		int mark2 = 74;
+		int mark3 = 92;
+		int average = (mark1 + mark2 + mark3)/3;
+		System.out.println("Average is " + average);
+	}
+}
